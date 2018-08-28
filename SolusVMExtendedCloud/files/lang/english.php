a8202855dadbb742731713e70f90a6d824c4a180
gzzchh <xjdzch@126.com>
2018-08-28 18:04:25 +0800

    Add SVM

--- SolusVMExtendedCloud/files/lang/english.php
+++ SolusVMExtendedCloud/files/lang/english.php
@@ -1,10 +1,10 @@
-<?php //ICB0 71:0                                                             ?><?php //004fb
-if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');if(function_exists('dl')){@dl($__ln);}if(function_exists('_il_exec')){return _il_exec();}$__ln='/ioncube/'.$__ln;$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}if(function_exists('dl')){@dl($__ln);}}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo("Site error: the ".(php_sapi_name()=='cli'?'ionCube':'<a href="http://www.ioncube.com">ionCube</a>')." PHP Loader needs to be installed. This is a widely used PHP extension for running ionCube protected PHP code, website security and malware blocking.\n\nPlease visit ".(php_sapi_name()=='cli'?'get-loader.ioncube.com':'<a href="http://get-loader.ioncube.com">get-loader.ioncube.com</a>')." for install assistance.\n\n");exit(199);
+<?php
+
+/*
+ * @ PHP 5.6
+ * @ Decoder version : 1.0.0.1
+ * @ Release on : 22.07.2018
+ * @ Website    : http://EasyToYou.eu
+ */
+
 ?>
-HR+cPvJGZWeb97CjC6eRANsZMIlMkyE7x41JFT9SE03BXxhl6Fk5s+/wqT4Nn392D3Frnbn/e1kB
-skgXm6azDI1l1oEh45CEIZgT7An/JPdyKarXJP841aV59/9Z/DWb0/iDkaT91BswGsRp7Kk1p8zk
-iHgIZRAybxxa0F16TMdzbL2NJyLxDLxZSpSmqaCJYTzj8G9msuu3nXUZieqbqzsFgDkoGkYi+DcT
-HOaNcG80qMiA1/cxpSBywOXsW1Z2Rp+0jwGJ/In39yODOCB3ECZEK2VpP54WoaTdeaLC03bq9DBl
-JbjFteOFDBPaThfYLR5q+HGalGFAMg7FH+SG6owoWvafh9QMPw18GpF/V3kUK7+7Xg/xUnJOQCPt
-PDsF4tOJWynlKKl0KX4O+CalJfZ1hhvBbPR7D1lt35NluaSC21/icmMxOcD1e10rIyJIOfs/Fbgy
-Yp9mom==
