d4fe1a80b15b5ae8bbea305ecebe3d90997c38b1
gzzchh <xjdzch@126.com>
2018-09-10 13:26:19 +0800

    补齐了DA扩展

---
+++ ModulesGarden_CHS-directadminextended-zh.php
@@ -0,0 +1,138 @@
+<?php
+
+$_LANG['token'] = '，错误令牌：';
+$_LANG['generalError'] = '出了点问题。检查日志并联系管理员';
+
+$_LANG['datatables']['next'] = '下一个';
+$_LANG['datatables']['previous'] = '以前';
+$_LANG['datatables']['zeroRecords'] = '无内容显示';
+
+$_LANG['pagesLabels']['functions']['label'] = '特征';
+$_LANG['pagesLabels']['home']['label'] = "仪表板";
+$_LANG['pagesLabels']['documentation']['label'] = "文档";
+
+$_LANG['pages']['functions']['group_label']['emails'] = "电子邮件";
+$_LANG['pages']['functions']['option_label']['emails'] = "电子邮件";
+$_LANG['pages']['functions']['option_label']['autoresponders'] = "自动回复";
+$_LANG['pages']['functions']['option_label']['vacation'] = "假期消息";
+$_LANG['pages']['functions']['option_label']['emailfilters'] = "垃圾邮件过滤器";
+$_LANG['pages']['functions']['option_label']['mailinglists'] = "邮件列表";
+$_LANG['pages']['functions']['option_label']['spamassasin'] = "Spamassasin设置";
+$_LANG['pages']['functions']['option_label']['email_forwarding'] = "电子邮件转发器";
+
+$_LANG['pages']['functions']['group_label']['login'] = "一键登录";
+$_LANG['pages']['functions']['option_label']['directadmin_login'] = "DirectAdmin";
+$_LANG['pages']['functions']['option_label']['webmail_login'] = "企业邮局";
+$_LANG['pages']['functions']['option_label']['login_phpmyadmin'] = "phpMyAdmin管理";
+
+$_LANG['pages']['functions']['group_label']['domains'] = "域名";
+$_LANG['pages']['functions']['option_label']['addon_domains'] = "添加域名";
+$_LANG['pages']['functions']['option_label']['domain_forwarders'] = "网站重定向";
+$_LANG['pages']['functions']['option_label']['subdomains'] = "子域名";
+$_LANG['pages']['functions']['option_label']['parked_domains'] = "域名指向";
+
+$_LANG['pages']['functions']['group_label']['others'] = "其他";
+$_LANG['pages']['functions']['option_label']['ftp'] = "FTP帐户";
+$_LANG['pages']['functions']['option_label']['databases'] = "数据库";
+$_LANG['pages']['functions']['option_label']['cron'] = "自动任务";
+$_LANG['pages']['functions']['option_label']['ssl'] = "SSL证书";
+$_LANG['pages']['functions']['option_label']['change_password'] = "更改密码";
+$_LANG['pages']['functions']['option_label']['apachehandlers'] = "Apache处理程序";
+$_LANG['pages']['functions']['option_label']['filemanager'] = "文件管理器";
+$_LANG['pages']['functions']['option_label']['fterrorpagesp'] = "自定义错误页面";
+$_LANG['pages']['functions']['option_label']['stats'] = "网站摘要";
+$_LANG['pages']['functions']['option_label']['perl_modules'] = "Perl模块";
+
+$_LANG['pages']['functions']['group_label']['backups'] = "备份";
+$_LANG['pages']['functions']['option_label']['backups'] = "备份";
+$_LANG['pages']['functions']['option_label']['recreate'] = "重新创建";
+
+$_LANG['pages']['functions']['group_label']['account_usage'] = "帐户使用情况";
+$_LANG['pages']['functions']['option_label']['usage_disk'] = "磁盘空间";
+$_LANG['pages']['functions']['option_label']['usage_bandwidth'] = "带宽";
+$_LANG['pages']['functions']['option_label']['usage_email'] = "电子邮件";
+$_LANG['pages']['functions']['option_label']['usage_ftp'] = "FTP帐户";  
+$_LANG['pages']['functions']['option_label']['usage_database'] = "数据库";
+
+$_LANG['pages']['functions']['table_header']['product'] = "产品";
+$_LANG['pages']['functions']['table_header']['configure'] = "操作";
+$_LANG['pages']['functions']['option_label']['copy_configuration_from'] = "复制配置来自";
+$_LANG['pages']['functions']['label']['copy_configuration'] = "复制配置";
+$_LANG['pages']['functions']['button']['copy'] = "复制";
+$_LANG['pages']['functions']['button']['features'] = "特征";
+$_LANG['pages']['functions']['button']['install_app'] = "应用";
+$_LANG['pages']['functions']['button']['save'] = "保存";
+$_LANG['pages']['functions']['button']['configuration'] = "组态";
+$_LANG['pages']['functions']['button']['back'] = "背部";
+$_LANG['pages']['functions']['label']['look_and_feel'] = "客户区模板";
+$_LANG['pages']['functions']['option_label']['skin'] = "模板";
+
+$_LANG['pages']['functions']['help']['install_app1'] = "使用Softaculous或Installatron安装应用程序！这个强大的功能将允许您的客户立即在他们的帐户上安装250多个应用程序之一！ ";
+$_LANG['pages']['functions']['help']['install_app2'] = "警告！它需要在DirectAdmin服务器上安装Softaculous / Installatron。有关详细信息，请访问Softaculous.com或Installatron.com";
+$_LANG['pages']['functions']['help']['conf_opt'] = "选择将为新创建的帐户自动安装的应用程序。";
+$_LANG['pages']['functions']['help']['lang'] = "通过输入两个字母的代码来更改应用程序的语言，例如'en'，'pl'。";
+
+$_LANG['pages']['functions']['info']['save_success'] = "配置已保存";
+
+$_LANG['pages']['functions']['table']['empty'] = "没有DirectAdmin Extended产品";
+
+$_LANG['pages']['functions']['table_header']['auto_installer'] = "自动安装程序";
+$_LANG['pages']['functions']['table_header']['template'] = "模板";
+$_LANG['pages']['functions']['table_header']['enabled_features'] = "启用功能";
+$_LANG['pages']['functions']['table']['enabled_features'] = "启用功能";
+
+$_LANG['pages']['home']['panel']['products'] = "制品";
+$_LANG['pages']['home']['panel']['servers'] = "服务器";
+$_LANG['pages']['home']['panel']['and'] = "和";
+$_LANG['pages']['home']['badge']['active'] = "活跃";
+$_LANG['pages']['home']['badge']['suspended'] = "暂停";
+$_LANG['pages']['home']['badge']['terminated'] = "终止";
+$_LANG['pages']['home']['badge']['active_accounts'] = "活跃账户";
+$_LANG['pages']['home']['info']['directadmin_products_migrate'] = '您可以轻松地将这些产品升级到DirectAdmin Extended。单击<a href="http://www.docs.modulesgarden.com/DirectAdmin_Extended_For_WHMCS#Management_of_Existing_Products" style="color: red;">此处</a>以查看如何执行此操作。';
+$_LANG['pages']['home']['label']['product_configuration'] = "产品配置";
+
+$_LANG['pages']['home']['info']['no_product_configured_yet'] = "尚未配置任何产品";
+$_LANG['pages']['home']['info']['no_servers_configured_yet'] = "尚未配置任何服务器";
+
+$_LANG['pages']['functions']['info']['directadmin_templates_location'] = "所有模板都位于：'/ your_whmcs_path / modules / servers / directadminExtended / views / client'。
+您可以通过以新名称复制当前模板来创建新模板。";
+$_LANG['pages']['functions']['help']['download'] = "单击此处下载应用程序列表";
+
+$_LANG['pages']['home']['info']['directadmin_upgrade_warning'] = "警告！<br/>似乎旧文件没有被正确删除！请确保删除位于'：addon_dir：'和'：server_dir：'目录中的所有文件，然后才复制新文件。注意：无需删除license.php文件。";
+
+$_LANG['pages']['functions']['label']['ftp_backups_end_points'] = "FTP备份端口";
+$_LANG['pages']['functions']['label']['backup_directory_paths'] = "备份目录路径";
+$_LANG['pages']['functions']['option_label']['none'] = "没有";
+$_LANG['pages']['functions']['option_label']['errorpages'] = "自定义错误页面";
+$_LANG['pages']['functions']['option_label']['install_app'] = "安装应用程序";
+$_LANG['pages']['functions']['option_label']['auto_installer'] = "自动安装程序";
+$_LANG['pages']['functions']['option_label']['softaculous'] = "Softaculous一键安装网站程序";
+$_LANG['pages']['functions']['option_label']['installatron'] = "安装";
+$_LANG['pages']['functions']['option_label']['assign_to_configurable_options'] = "分配给可配置选项";
+$_LANG['pages']['functions']['option_label']['apps_configurable_options'] = "应用程序可配置选项";
+$_LANG['pages']['functions']['option_label']['install_button'] = "安装按钮";
+$_LANG['pages']['functions']['option_label']['backup_button'] = "备份按钮";
+$_LANG['pages']['functions']['option_label']['language'] = "语言";
+$_LANG['pages']['functions']['label']['ftp_backup_end_points'] = "FTP备份端口";
+$_LANG['pages']['functions']['end_points_table']['server'] = "服务器";
+$_LANG['pages']['functions']['end_points_table']['backup_name'] = "备份名称";
+$_LANG['pages']['functions']['end_points_table']['host'] = "地址";
+$_LANG['pages']['functions']['end_points_table']['port'] = "端口";
+$_LANG['pages']['functions']['end_points_table']['user'] = "用户";
+$_LANG['pages']['functions']['end_points_table']['password'] = "密码";
+$_LANG['pages']['functions']['end_points_table']['path_to_files'] = "文件路径";
+$_LANG['pages']['functions']['end_points_table']['use_admin_access'] = "使用Admin访问";
+$_LANG['pages']['functions']['end_points_table']['enable_restores'] = "启用还原";
+$_LANG['pages']['functions']['end_points_table']['actions'] = "操作";
+$_LANG['pages']['functions']['remove'] = "移除";
+$_LANG['pages']['functions']['add'] = "添加";
+$_LANG['pages']['functions']['end_points_table']['name'] = "名称";
+$_LANG['pages']['functions']['end_points_table']['path'] = "路径";
+$_LANG['pages']['functions']['end_points_table']['enable_path'] = "启用路径";
+$_LANG['pages']['functions']['option_label']['none'] = "没有";
+
+$_LANG['pages']['functions']['label']['other'] = "其他";
+$_LANG['pages']['functions']['option_label']['webmail_type'] = "网络邮件类型";
+$_LANG['pages']['functions']['option_label']['show_server_ip'] = "显示服务器IP";
+$_LANG['pages']['functions']['option_label']['show_ssl_encrypt'] = "显示Let's Encrypt 按钮";
+
